b795b008e1c6f321a6531e8e2a6ef83dbffdedf6
MatteoArcangeli <matteoa1999@gmail.com>
2021-03-14 11:31:47 +0100

    first commit

---
+++ App/android/app/src/main/kotlin/com/project/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.project.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ App/lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.mcLarenTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> aule = [
+    "Aula 1",
+    "Aula 2",
+    "Laboratorio 1",
+    "Sala studio 1",
+    "Aula 3",
+    "Sala studio 2"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  Widget build(BuildContext context) {
+    final title = 'Selezionare l\'aula';
+    return MaterialApp(
+        title: title,
+        home: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text(
+                title,
+                style: GoogleFonts.mcLaren(),
+              ),
+              backgroundColor: Colors.blueGrey,
+            ),
+            body: Column(
+              children: <Widget>[
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    children: List.generate(6, (index) {
+                      return Center(
+                        child: SizedBox(
+                          width: 150.0,
+                          height: 100.0,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.blueGrey,
+                              ),
+                              onPressed: () {
+                                if (prova(index))
+                                  awesomeDialog(context, "BOTTONE PREMUTO",
+                                      "Hai premuto il bottone $index");
+                              },
+                              child: RichText(
+                                text: TextSpan(
+                                  style: GoogleFonts.mcLaren(),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                        text: aule[index],
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold)),
+                                  ],
+                                ),
+                              )),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+                Image.asset('assets/unicam.png', scale: 1.5),
+                SizedBox(height: 10),
+              ],
+            )));
+  }
+
+  bool prova(int index) {
+    //if(index%2==0)
+    return true;
+    // return false;
+  }
+
+  AwesomeDialog awesomeDialog(
+      BuildContext context, String titolo, String descrizione) {
+    return AwesomeDialog(
+      context: context,
+      headerAnimationLoop: true,
+      animType: AnimType.SCALE,
+      title: titolo,
+      desc: descrizione,
+      autoHide: Duration(seconds: 5),
+    )..show();
+  }
+}
