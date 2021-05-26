f11f7991fa562ab096731197adeb3cec852c5433
Utente17 <matteoa1999@gmail.com>
2021-05-26 14:50:17 +0200

    Rivisto backend drone secondario

--- backendPrimoPiano/api/routes/drone.js
+++ backendPrimoPiano/api/routes/drone.js
@@ -1,93 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
-const {PythonShell} =require('python-shell'); 
-  
+const {
+  PythonShell
+} = require('python-shell');
+
 var occupato = false;
 
-router.get("/stato", (_req, res)=>{ 
-  console.log(occupato);
-  if(occupato) {
+/*
+ * Invoca lo sciprt Python passato come parametro
+ */
+function invokePythonScript(scriptName) {
+  let options = {
+    mode: 'text',
+    pythonPath: '/usr/bin/python',
+    pythonOptions: ['-u'],
+    scriptPath: '/home/clover/Desktop/backend/api/script',
+  };
+
+  PythonShell.run(scriptName, options, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+router.get("/stato", (_req, res) => {
+  if (occupato) {
     return res.sendStatus(403);
-  }
-  else
-  {
-    //occupato = true;
+  } else {
+    occupato = true;
     return res.sendStatus(200);
   }
 });
 
-router.get("/aula1", (_req, res)=>{ 
-  let options = {
-    mode: 'text',
-    pythonPath: '/usr/bin/python',
-    pythonOptions: ['-u'],
-    scriptPath: '/home/clover/Desktop/backend/api/script',
-  };
-
-PythonShell.run('aula1.py', options, function (err) {
-    if (err){
-      res.sendStatus(500, err.message);
-    }
-  });
-
-  occupato=false;
-});
-
-router.get("/aula2", (_req)=>{ 
-  occupato=true;
-
-  let options = {
-    mode: 'text',
-    pythonPath: '/usr/bin/python',
-    pythonOptions: ['-u'],
-    scriptPath: '/home/clover/Desktop/backend/api/script',
-  };
-
-PythonShell.run('aula2.py', options, function (err) {
-    if (err){
-      console.log(err);
-    }
-  });
-
-  occupato=false;
-});
-
-router.get("/aula3", (_req)=>{ 
-  occupato=true;
-
-  let options = {
-    mode: 'text',
-    pythonPath: '/usr/bin/python',
-    pythonOptions: ['-u'],
-    scriptPath: '/home/clover/Desktop/backend/api/script',
-  };
-
-PythonShell.run('aula3.py', options, function (err) {
-    if (err){
-      console.log(err);
-    }
-  });
-
-  occupato=false;
-});
-
-router.get("/laboratorio", (_req)=>{ 
-  occupato=true;
-
-  let options = {
-    mode: 'text',
-    pythonPath: '/usr/bin/python',
-    pythonOptions: ['-u'],
-    scriptPath: '/home/clover/Desktop/backend/api/script',
-  };
-
-PythonShell.run('laboratorio.py', options, function (err) {
-    if (err){
-      console.log(err);
-    }
-  });
-  occupato=false;
+router.get("/aula1", (_req, res) => {
+  invokePythonScript('aula1');
+  occupato = false;
 });
 
 module.exports = router;
